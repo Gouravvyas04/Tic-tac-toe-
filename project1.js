2c2fbb7a65a5f228672245e59093845da45a8602
Gourav Vyas <160164280+Gouravvyas04@users.noreply.github.com>
2025-02-11 20:48:40 +0530

    Update project1.js

--- project1.js
+++ project1.js
@@ -3,6 +3,7 @@ let resetBtn = document.querySelector("#reset");
 let newBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector(".msg");
+
 let turnO = true;
 const winPatterns= [
     [0,1,2], [0,4,8], [0,3,6], [2,4,6], [3,4,5], [6,7,8], [1,4,7], [2,5,8]
